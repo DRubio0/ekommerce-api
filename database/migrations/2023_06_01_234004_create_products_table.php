2613e8f60776732cdbb23e67e9e71c92170a667f
Daniel Rubio <rubio.krkmo@gmail.com>
2023-06-01 17:54:15 -0600

    subiendo proyecto

---
+++ database/migrations/2023_06_01_234004_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price',7,2);
+            $table->integer('stock');
+            $table->string('brand');
+            $table->string('image');
+            $table->text('description');
+            $table->string('sku');
+            $table->boolean('state');
+            // Referencia a la tabla categories
+            $table->unsignedBigInteger('subcategory_id');
+            $table->foreign('subcategory_id')->references('id')->on('subcategories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
