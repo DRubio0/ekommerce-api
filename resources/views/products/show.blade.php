7abf47b1e8ec8fe9d1090953eb9ff58f64470cb6
Daniel Rubio <rubio.krkmo@gmail.com>
2023-06-06 18:21:19 -0600

    add code to show.blade

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -9,20 +9,28 @@
         class="flex flex-col justify-center w-full items-center h-screen mx-auto bg-gradient-to-tr from-blue-700 via-blue-800 to-gray-900 gap-6 shadow-2xl">
         <h1 class="font-bold uppercase text-xl text-white">Product</h1>
         <div class="bg-white">
-            <div class="max-w-sm rounded overflow-hidden shadow-lg">
-                <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains">
-                <div class="px-6 py-4">
-                  <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-                  <p class="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                  </p>
+            
+                <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                  
+                        <img class="flex" width="150px" height="150px" src="{{ asset('storage/' . $product->image) }}" alt="{{$product->name}}">
+                    
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2">{{$product->name}}</div>
+                        <p class="text-gray-700 text-base">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
+                            perferendis eaque, exercitationem praesentium nihil.
+                        </p>
+                    </div>
+                    <div class="px-6 pt-4 pb-2">
+                        <span
+                            class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                        <span
+                            class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                        <span
+                            class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                    </div>
                 </div>
-                <div class="px-6 pt-4 pb-2">
-                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                </div>
-              </div>
+       
         </div>
     </div>
 @endsection
