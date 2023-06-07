4ba15edcd66761bc0a099b95b958a84ce91366dc
Majogobla <marcosjgodoyb@gmail.com>
2023-06-06 23:09:35 -0600

    CRUD Products Completed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,16 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Str;
 use App\Models\Product;
 use App\Models\Subcategories;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index(Request $request)
     {
         $view = $request->path();
@@ -21,13 +17,8 @@ class ProductController extends Controller
             'products' => $products,
             'view' => $view,
         ]);
-
-
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create(Request $request)
     {
         $view = $request->path();
@@ -39,9 +30,6 @@ class ProductController extends Controller
         ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $this->validate(
@@ -51,7 +39,7 @@ class ProductController extends Controller
                 'price' => ['required'],
                 'stock' => ['required'],
                 'brand' => ['required'],
-                'image' => ['image'],
+                'image'=>['required', 'image'],
                 'description' => ['required'],
                 'sku' => ['required'],
                 'subcategory_id' => ['required'],
@@ -59,7 +47,6 @@ class ProductController extends Controller
         );
 
         $imageName = $request->file('image')->store('img_product', 'public');
-
 
         Product::create([
             'name' => $request->name,
@@ -73,14 +60,9 @@ class ProductController extends Controller
             'subcategory_id' => $request->subcategory_id,
         ]);
 
-
         return redirect()->route('product.index');
-
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Request $request,string $id)
     {
         $view = $request->path();
@@ -92,21 +74,13 @@ class ProductController extends Controller
                 'view'=>$view,
             ]
         );
-
-
-
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Request $request, string $id)
     {
         $view = $request->path();
         $subcategories = Subcategories::all();
         $product = Product::where('id', $id)->get();
-
-        // dd($product);
 
         return view('products.edit', [
             'product' => $product[0],
@@ -115,9 +89,6 @@ class ProductController extends Controller
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         $this->validate(
@@ -127,18 +98,21 @@ class ProductController extends Controller
                 'price' => ['required'],
                 'stock' => ['required'],
                 'brand' => ['required'],
-                // 'image'=>[''],
+                'image'=>['required', 'image'],
                 'description' => ['required'],
                 'sku' => ['required'],
                 'subcategory_id' => ['required'],
             ]
         );
+
+        $imageName = $request->file('image')->store('img_product', 'public');
+
         $product = Product::findOrFail($id);
         $product->name = $request->name;
         $product->price = $request->price;
         $product->stock = $request->stock;
         $product->brand = $request->brand;
-        // $product->image = $request->image;
+        $product->image = $imageName;
         $product->description = $request->description;
         $product->sku = $request->sku;
         $product->subcategory_id = $request->subcategory_id;
@@ -147,16 +121,11 @@ class ProductController extends Controller
 
         return redirect()->route('product.index');
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
+    
     public function destroy($id)
     {
         $product = Product::findOrFail($id);
         $product->delete();
         return redirect()->route('product.index');
-
-
     }
 }
