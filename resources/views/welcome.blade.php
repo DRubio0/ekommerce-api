abdc1bffe06aefb85863bac39d434b084d9e811c
Daniel Rubio <rubio.krkmo@gmail.com>
2023-06-06 22:08:46 -0600

    change welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,25 @@
 @extends('layouts.app')
 
-@section('title')
-
-@endsection
-
 @section('content')
-<div class="bg-blue-100 text-center justify-center p-11">
-    <h1 class="mb-8">Hola</h1>
-    <a class="bg-red-300 p-2 mt-4" href="{{route('product.index')}}">Products</a>
+<div class="flex">
+    <div class="w-1/5 bg-gray-800 text-white">
+        <ul class="py-4">
+            <li>
+                <a href="" class="block px-4 py-2 hover:bg-gray-700">Dashboard</a>
+            </li>
+            <li>
+                <a href="" class="block px-4 py-2 hover:bg-gray-700">Product</a>
+            </li>
+            <li>
+                <a href="" class="block px-4 py-2 hover:bg-gray-700">Users</a>
+            </li>
+        </ul>
+    </div>
+    <div class="w-4/5 bg-white">
+        <div class="px-4 py-2">
+                <h2 class="text-2xl font-bold">Product View</h2>
+                <!-- Contenido de la vista de productos -->
+        </div>
+    </div>
 </div>
 @endsection
