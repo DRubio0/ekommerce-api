5573c8a7e6338ea147d5186e53e3edba72f0de5f
Daniel Rubio <rubio.krkmo@gmail.com>
2023-06-12 18:07:14 -0600

    change view to product show

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,12 +47,11 @@
                 </li>
                 <li>
                     <a href="{{ route('product.index') }}" class="block px-4 py-2 hover:bg-gray-700">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block mr-2" fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-                        </svg>
-                        Product
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 inline-block mr-2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12" />
+                          </svg>
+                          
+                        Products
                     </a>
                 </li>
                 <li>
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -7,51 +7,36 @@
 @section('content')
     <div class="flex justify-center ">
         <div class="max-w-lg w-auto mt-3 bg-blue-100/20 p-6 rounded-xl">
-            <h1 class="font-bold uppercase text-4xl text-center mb-4">Product View</h1>
-            <div class="max-w-sm bg-gray-200 border border-gray-200 rounded-2xl">
+            
+            <h1 class="font-bold uppercase text-4xl text-center mb-4">Show Product {{$product->id}}</h1>
+            <div
+                class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <a href="#">
-                    <img class="rounded-t-lg" src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" />
+                    <img class="p-8 rounded-t-lg" src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}"/>
                 </a>
-                <div class="p-5">
-                    <h2 class="mb-2 uppercase text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        {{ $product->name }}</h2>
-                    <div class="flex justify-between items-center">
-                        <p class="text-gray-500 uppercase font-bold">
-                            Brand:
-                            {{ $product->brand }}
-                        </p>
-                        <p class="text-gray-500 uppercase font-bold">
-                            SKU: {{ $product->sku }}
-                        </p>
+                <div class="px-5 pb-5">
+                    <a href="#">
+                        <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ $product->name}}</h5>
+                    </a>
+                    <div class="flex  mt-2.5 mb-5 justify-between items-center">
+                        <div>
+                            Brand: {{$product->brand}} 
+                        </div>
+                        <div>
+                            Category: {{$product->subcategory->name}}
+                        </div>
                     </div>
-                    <div class=" mt-1 bg-gray-200 hover:bg-gray-300 pl-3 pb-1.5 border-2 border-blue-500 rounded">
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                        <h4 class=" font-bold text-lg"> Description:</h4>
-                        <p class="text-lg font-bold text-justify">
-                            {{ $product->description }}
-                        </p>
-                        </p>
+                    <div class="flex items-center mt-2.5 mb-5 text-justify lowercase font-bold">
+                        {{$product->description}}
                     </div>
-                    <div class=" mt-1 bg-gray-200 pl-3 pb-1.5 border-2 border-blue-500 rounded">
-                        <h4 class=" font-bold text-lg">Price:</h4>
-                        <p class="text-lg  font-bold">
-                            $ {{ $product->price }}
-                        </p>
+                    <div class="flex items-center justify-between">
+                        <span class="text-3xl font-bold text-gray-900 dark:text-white">${{$product->price}}</span>
+                        <a href="{{route('product.index')}}"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Return</a>
                     </div>
-                    <div class=" mt-1 mb-2 bg-gray-200 pl-3 pb-1.5 border-2 border-blue-500 rounded">
-                        <h4 class=" font-bold text-lg">Category:
-                            <a class="text-lg  font-bold">{{ $product->subcategory->name }} </a>
-                        </h4>
-                    </div>
-                    <div class="flex justify-center">
-                        <a href="{{ route('product.index') }}"
-                            class=" text-xl inline-flex items-center px-3 py-2 font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-yellow-600 transition-colors focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">
-                            Return
-                        </a>
-                    </div>
-
                 </div>
             </div>
+           
         </div>
     </div>
 @endsection
